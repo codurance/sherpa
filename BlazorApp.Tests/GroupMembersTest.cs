948419c10dbe8e1010834da11531a16a43ecb2bc
IsmaelSendros <ismael.sendros@codurance.com>
2023-04-04 14:32:10 +0200

    test: adding a second test to validate empty last names are allowed

--- BlazorApp.Tests/GroupMembersTest.cs
+++ BlazorApp.Tests/GroupMembersTest.cs
@@ -1,20 +1,38 @@
 using BlazorApp.Model;
+using BlazorApp.Pages;
 using Bunit;
 
 namespace BlazorApp.Tests;
 
 public class GroupMembersTest
 {
- 
+
+    private TestContext _testContext;
+    private IRenderedComponent<GroupMemberTable> _renderedComponent;
+
+    public GroupMembersTest()
+    {
+        _testContext = new TestContext();
+    }
+    
     [Fact]
     public void AssertFirstElementOfTheListShowsTheRightValues()
     {
         var _groupMember = new GroupMember("Bob","Smith","CEO");
+        _renderedComponent = _testContext.RenderComponent<Pages.GroupMemberTable>(b => 
+            b.Add( e => e.GroupMembers, new List<GroupMember>{_groupMember}));
         
-        using var context = new TestContext();
-        var renderedComponent = context.RenderComponent<Pages.GroupMemberTable>(b => 
+        Assert.Equal(_groupMember.Name,_renderedComponent.Instance.GroupMembers[0].Name);
+    }
+
+    [Fact]
+    public void AssertLastNameCanBeEmpty()
+    {
+        var _groupMember = new GroupMember("Bob",null,"CEO");
+        var renderedComponent = _testContext.RenderComponent<Pages.GroupMemberTable>(b => 
             b.Add( e => e.GroupMembers, new List<GroupMember>{_groupMember}));
 
-        Assert.Equal(_groupMember.Name1,renderedComponent.Instance.GroupMembers[0].Name1);
+        Assert.Equal(_groupMember.LastName,renderedComponent.Instance.GroupMembers[0].LastName);
     }
+    
 }
--- BlazorApp/Model/GroupMember.cs
+++ BlazorApp/Model/GroupMember.cs
@@ -2,32 +2,14 @@ namespace BlazorApp.Model;
 
 public class GroupMember
 {
-    private String Name;
-    private String LastName;
-    private String Position;
+    public String Name { get; set; }
+    public String LastName { get; set; }
+    public String Position { get; set; }
 
     public GroupMember(string name, string lastName, string position)
     {
         Name = name;
         LastName = lastName;
         Position = position;
-    }
-
-    public string Name1
-    {
-        get => Name;
-        set => Name = value ?? throw new ArgumentNullException(nameof(value));
-    }
-
-    public string LastName1
-    {
-        get => LastName;
-        set => LastName = value ?? throw new ArgumentNullException(nameof(value));
-    }
-
-    public string Position1
-    {
-        get => Position;
-        set => Position = value ?? throw new ArgumentNullException(nameof(value));
     }
 }
