2a0886ede1914e42f3d2dc60e559dcef95b06dd4
JavierRLab <jrlabra2@gmail.com>
2023-04-04 10:52:03 +0200

    first blazor tests running

--- BlazorApp.Tests/UnitTest1.cs
+++ BlazorApp.Tests/UnitTest1.cs
@@ -1,9 +1,23 @@
+using Bunit;
+
 namespace BlazorApp.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void shouldTrue()
     {
+        Assert.True(true);
+    }
+    
+    [Fact]
+    public void Basic_Markup_IndexComponentRendersCorrectly()
+    {
+        using var context = new TestContext();
+
+        var renderedComponent = context.RenderComponent<Pages.Index>();
+
+        renderedComponent.MarkupMatches(
+            "<h1>Hello, world!</h1>\r\n\r\nWelcome to your new app.");
     }
 }
