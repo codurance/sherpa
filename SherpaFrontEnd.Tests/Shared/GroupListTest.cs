7cef535f3294186647141de0b2dc913dc7521120
IsmaelSendros <ismael.sendros@codurance.com>
2023-04-12 09:59:54 +0200

    test: fixing broken test (sorry)

--- SherpaFrontEnd.Tests/Shared/GroupListTest.cs
+++ SherpaFrontEnd.Tests/Shared/GroupListTest.cs
@@ -1,7 +1,10 @@
 ﻿using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using SherpaFrontEnd.Model;
 using SherpaFrontEnd.Shared;
 using SherpaFrontEnd.Pages;
+using SherpaFrontEnd.Services;
 
 namespace BlazorApp.Tests.Shared;
 
@@ -10,16 +13,21 @@ public class GroupListTest
 
     private TestContext _testContext;
     private IRenderedComponent<GroupsList> _renderedComponent;
+    private Mock<IGroupDataService> _mockGroupDataService;
 
     public GroupListTest()
     {
         _testContext = new TestContext();
+        _mockGroupDataService = new Mock<IGroupDataService>();
+        _testContext.Services.AddScoped(p => _mockGroupDataService.Object);
     }
 
     [Fact]
     public void AsssertThatListOfGroupsIsRendered()
     {
-        var group = new Group("group1");
+        var group = new Group("Group A");
+        _mockGroupDataService.Setup(groupService => groupService.getGroups())
+            .Returns(new List<Group> { new Group("Group A") });
         
         _renderedComponent = _testContext.RenderComponent<GroupsList>(b => b.Add(b => b.groups,new List<Group>{group}));
         
