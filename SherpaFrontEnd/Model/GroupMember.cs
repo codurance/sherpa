b977d5825070c22c9a61cddcf3c57875795b3d6f
JavierRLab <jrlabra2@gmail.com>
2023-04-21 14:58:21 +0200

    feat: adding functionality to add member button

--- SherpaFrontEnd/Model/GroupMember.cs
+++ SherpaFrontEnd/Model/GroupMember.cs
@@ -3,19 +3,24 @@ namespace SherpaFrontEnd.Model;
 public class GroupMember
 {
     
-    public string Name { get; set; }
-    public string LastName { get; set; }
-    public string Position { get; set; }
+    public string? Name { get; set; }
+    public string? LastName { get; set; }
+    public string? Position { get; set; }
     
-    public string Email { get; set; }
+    public string? Email { get; set; }
 
     public bool IsEditable { get; set; } = false;
 
-    public GroupMember(string name, string lastName, string position, string email)
+    public GroupMember(string? name, string? lastName, string? position, string? email)
     {
         Name = name;
         LastName = lastName;
         Position = position;
         Email = email;
+    }
+
+    public GroupMember()
+    {
+        
     }
 }
