c49055edeeaa4610dfa166bfa7c891e80da2c1e1
Moises Rodriguez <moisesrj97@gmail.com>
2023-07-31 11:04:41 +0200

    feat: templates controller returns 500 if service throws

--- SherpaBackEnd.Tests/Controllers/TemplateControllerTest.cs
+++ SherpaBackEnd.Tests/Controllers/TemplateControllerTest.cs
@@ -12,15 +12,28 @@ public class TemplateControllerTest
     public async void Should_call_service_and_return_service_returned_object()
     {
         var template = new Template(Guid.NewGuid(), "test", new Question[] { }, 10);
-        var arrayWithTemplate = new []{template};
-        
+        var arrayWithTemplate = new[] { template };
+
         var templateService = new Mock<ITemplateService>();
         templateService.Setup(service => service.GetAllTemplates()).ReturnsAsync(arrayWithTemplate);
         var templateController = new TemplateController(templateService.Object);
-        
+
         var templatesRequest = await templateController.GetAllTemplates();
         var templatesResult = Assert.IsType<OkObjectResult>(templatesRequest.Result);
         var actualTemplates = Assert.IsAssignableFrom<IEnumerable<Template>>(templatesResult.Value);
         Assert.Equal(arrayWithTemplate, actualTemplates);
+    }
+
+    [Fact]
+    public async void Should_return_status_code_500_if_some_error_is_thrown()
+    {
+        var templateService = new Mock<ITemplateService>();
+        templateService.Setup(service => service.GetAllTemplates()).ThrowsAsync(new Exception());
+        var templateController = new TemplateController(templateService.Object);
+
+        var templatesRequest = await templateController.GetAllTemplates();
+
+        var templatesResult = Assert.IsType<StatusCodeResult>(templatesRequest.Result);
+        Assert.Equal(500, templatesResult.StatusCode);
     }
 }
