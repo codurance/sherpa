6ba3434105914811f2e93c4d53e919f56193b568
Arturo Menendez <arturo.menendez@codurance.com>
2023-08-02 17:25:38 +0200

    feat: new acceptance test to navigate to Teams page and see its components

--- SherpaFrontEnd.Tests/Pages/TeamsAcceptanceTest.cs
+++ SherpaFrontEnd.Tests/Pages/TeamsAcceptanceTest.cs
@@ -1,22 +1,33 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
 using System.Net.Sockets;
+using Blazored.Modal;
 using Blazored.Modal.Services;
 using Bunit;
 using Bunit.TestDoubles;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Moq.Protected;
+using SherpaFrontEnd;
 using SherpaFrontEnd.Model;
 using SherpaFrontEnd.Pages;
 using SherpaFrontEnd.Services;
 using SherpaFrontEnd.Shared.Modals;
+using Xunit.Abstractions;
 
 namespace BlazorApp.Tests.Pages;
 
 public class TeamsAcceptanceTest
 {
+    private ITestOutputHelper output;
+
+    public TeamsAcceptanceTest(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
     [Fact]
     async Task should_be_able_to_create_team()
     {
@@ -25,13 +36,14 @@ public class TeamsAcceptanceTest
         var team = new Group(teamId, teamName);
 
         var testCtx = new TestContext();
-        var httpHandlerMock = new Mock<HttpMessageHandler>();
+        var httpHandlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
 
         var factoryHttpClient = new Mock<IHttpClientFactory>();
         var teamsService = new GroupServiceHttpClient(factoryHttpClient.Object);
         testCtx.Services.AddSingleton<IGroupDataService>(teamsService);
-        
-        var httpClient = new HttpClient(httpHandlerMock.Object, false) { BaseAddress = new Uri("http://localhost") };
+
+        const string baseUrl = "http://localhost";
+        var httpClient = new HttpClient(httpHandlerMock.Object, false) { BaseAddress = new Uri(baseUrl) };
         factoryHttpClient.Setup(_ => _.CreateClient("SherpaBackEnd")).Returns(httpClient);
 
         var emptyTeamsList = new List<Group>(){new Group(teamId, "monda")};
@@ -51,6 +63,20 @@ public class TeamsAcceptanceTest
                 ItExpr.IsAny<CancellationToken>())
             .ReturnsAsync(responseEmpty);
 
+        var teamJson = await JsonContent.Create(team).ReadAsStringAsync();
+        var createdTeamResponse = new HttpResponseMessage()
+        {
+            StatusCode = HttpStatusCode.Created,
+        };
+        
+        httpHandlerMock
+            .Protected()
+            .SetupSequence<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.Is<HttpRequestMessage>(
+                    m => m.Content != null && m.Method.Equals(HttpMethod.Post) && m.Content.Equals(teamJson) && m.RequestUri!.AbsoluteUri.Contains("/groups")),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(createdTeamResponse);
 
         var teamsPage = testCtx.RenderComponent<GroupsList>();
 
@@ -63,21 +89,58 @@ public class TeamsAcceptanceTest
         var confirmTeamCreationButton = teamsPage.Find("#create-team-confirm");
         confirmTeamCreationButton.Click();
 
-        var teamJson = await JsonContent.Create(team).ReadAsStringAsync();
-        var createdTeamResponse = new HttpResponseMessage()
+
+        var navMan = testCtx.Services.GetRequiredService<FakeNavigationManager>();
+        Assert.Equal( $"{baseUrl}/group-content/{teamId}", navMan.Uri);
+        
+    }
+
+    [Fact]
+    async Task shouldBeAbleToNavigateToTeamsPageAndSeeItsComponents()
+    {
+        var testCtx = new TestContext();
+        testCtx.Services.AddBlazoredModal();
+
+        var httpHandlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+
+        var factoryHttpClient = new Mock<IHttpClientFactory>();
+        var teamsService = new GroupServiceHttpClient(factoryHttpClient.Object);
+        testCtx.Services.AddSingleton<IGroupDataService>(teamsService);
+
+        const string baseUrl = "http://localhost";
+        var httpClient = new HttpClient(httpHandlerMock.Object, false) { BaseAddress = new Uri(baseUrl) };
+        factoryHttpClient.Setup(_ => _.CreateClient("SherpaBackEnd")).Returns(httpClient);
+
+        var emptyTeamsList = new List<Group>(){};
+        var emptyTeamListJson = await JsonContent.Create(emptyTeamsList).ReadAsStringAsync();
+        var responseEmpty = new HttpResponseMessage()
         {
-            StatusCode = HttpStatusCode.Created,
+            StatusCode = HttpStatusCode.OK,
+            Content = new StringContent(emptyTeamListJson)
         };
+
         httpHandlerMock
             .Protected()
             .SetupSequence<Task<HttpResponseMessage>>(
                 "SendAsync",
                 ItExpr.Is<HttpRequestMessage>(
-                    m => m.Content != null && m.Method.Equals(HttpMethod.Post) && m.Content.Equals(teamJson)),
+                    m => m.Method.Equals(HttpMethod.Get)),
                 ItExpr.IsAny<CancellationToken>())
-            .ReturnsAsync(createdTeamResponse);
+            .ReturnsAsync(responseEmpty);
+        
+        var appComponent = testCtx.RenderComponent<App>();
 
-        var navMan = testCtx.Services.GetRequiredService<FakeNavigationManager>();
-        Assert.Equal(httpClient.BaseAddress + $"group-content/{teamId}", navMan.Uri);
+        var targetPage = "groups-list-page";
+        var teamsPageLink = appComponent.Find($"a[href='{targetPage}']");
+        Assert.NotNull(teamsPageLink);
+        var navManager = testCtx.Services.GetRequiredService<FakeNavigationManager>();
+        navManager.NavigateTo($"/{targetPage}");
+        output.WriteLine(appComponent.Markup);
+
+        var allTeamsTitle = appComponent.FindAll("h1,h2,h3").FirstOrDefault(element => element.InnerHtml.Contains("All teams"));
+        Assert.NotNull(allTeamsTitle);
+        
+        var createTeamButton = appComponent.Find("#create-team");
+        Assert.NotNull(createTeamButton);
     }
 }
