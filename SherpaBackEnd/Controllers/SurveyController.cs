37ee833f0ec51e1a9842ba561b7a14eb0827315f
Arturo Menendez <arturo.menendez@codurance.com>
2023-08-09 10:46:47 +0200

    feat: added suervey acceptance test. Created survey Dto

--- SherpaBackEnd/Controllers/SurveyController.cs
+++ SherpaBackEnd/Controllers/SurveyController.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using SherpaBackEnd.Dtos;
+using SherpaBackEnd.Model.Survey;
+using SherpaBackEnd.Services;
+
 namespace SherpaBackEnd.Controllers;
 
 public class SurveyController
 {
-    
+    public SurveyController(SurveysService surveysService)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ActionResult> CreateSurvey(CreateSurveyDto createSurveyDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ActionResult<Survey>> GetSurveyById(Guid surveyId)
+    {
+        throw new NotImplementedException();
+    }
 }
--- SherpaBackEnd/Services/SurveysService.cs
+++ SherpaBackEnd/Services/SurveysService.cs
@@ -1,12 +1,19 @@
 ﻿using SherpaBackEnd.Model;
+using SherpaBackEnd.Repositories;
 
 namespace SherpaBackEnd.Services;
 
 public class SurveysService : ISurveysService
 {
     private readonly ISurveyRepository _surveyRepository;
-    public SurveysService(InMemorySurveyRepository surveyRepository)
+    private readonly InMemoryTeamRepository _inMemoryTeamRepository;
+    private readonly InMemoryFilesTemplateRepository _inMemoryFilesTemplateRepository;
+
+    public SurveysService(InMemorySurveyRepository surveyRepository, InMemoryTeamRepository inMemoryTeamRepository,
+        InMemoryFilesTemplateRepository inMemoryFilesTemplateRepository)
     {
         _surveyRepository = surveyRepository;
+        _inMemoryTeamRepository = inMemoryTeamRepository;
+        _inMemoryFilesTemplateRepository = inMemoryFilesTemplateRepository;
     }
 }
