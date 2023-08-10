3efb4e28d727e19012809a0b03a79dccd55c083c
Moises Rodriguez <moisesrj97@gmail.com>
2023-08-10 16:41:01 +0200

    feat: add dto

---
+++ SherpaFrontEnd/Dtos/SurveyWithoutQuestions.cs
@@ -0,0 +1,34 @@
+﻿
+using SherpaBackEnd.Model;
+using SherpaBackEnd.Model.Survey;
+using SherpaFrontEnd.Model;
+using SherpaFrontEnd.Services;
+
+namespace SherpaFrontEnd.Dtos;
+
+public class SurveyWithoutQuestions
+{
+    public Guid Id {get; set;}
+    public User Coach {get; set;}
+    public Status Status {get; set;}
+    public DateTime? Deadline {get; set;}
+    public string Title {get; set;}
+    public string? Description {get; set;}
+    public Response[] Responses {get; set;}
+    public Team Team {get; set;}
+    public TemplateWithoutQuestions Template {get; set;}
+    
+
+    public SurveyWithoutQuestions(Guid id, User coach, Status status, DateTime? deadline, string title, string? description, Response[] responses, Team team, TemplateWithoutQuestions template)
+    {
+        Id = id;
+        Coach = coach;
+        Status = status;
+        Deadline = deadline;
+        Title = title;
+        Description = description;
+        Responses = responses;
+        Team = team;
+        Template = template;
+    }
+}
---
+++ SherpaFrontEnd/Services/ISurveyService.cs
@@ -0,0 +1,11 @@
+using Shared.Survey;
+using SherpaFrontEnd.Dtos;
+
+namespace BlazorApp.Tests.Pages;
+
+public interface ISurveyService
+{
+    public Task CreateSurvey(CreateSurveyDto createSurveyDto);
+
+    public Task<SurveyWithoutQuestions> GetSurveyById(string id);
+}
