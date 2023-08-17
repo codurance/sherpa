f54224feab3f939c7b30a0922770b6bdfab1e3a5
NC-1234 <nick.choudhry@codurance.com>
2023-08-17 16:57:03 +0100

    feat: Implement survey table along with dynamic rows

--- SherpaFrontEnd.Tests/Pages/TeamContentTest.cs
+++ SherpaFrontEnd.Tests/Pages/TeamContentTest.cs
@@ -160,4 +160,42 @@ public class TeamContentTest
         Assert.NotNull(participants);
         Assert.NotNull(status);
     }
+
+        [Fact]
+        public void ShouldDisplayCorrectSurveyDataInUserTable()
+        {
+            var userOne = new User(Guid.NewGuid(), "testUser");
+            const string newTeamName = "Team with survey";
+            var testTeamId = Guid.NewGuid();
+            var testTeam = new Team(testTeamId, newTeamName);
+            
+            var testSurvey = new List<Survey>
+            {
+            new Survey(
+                    Guid.NewGuid(), 
+                    userOne, 
+                    Status.Draft, 
+                    new DateTime(), 
+                    "title", 
+                    "description",
+                    Array.Empty<Response>(), 
+                    testTeam, 
+                    new Template("template")
+                )
+            };
+
+            var surveyTableComponent = _testContext.RenderComponent<SurveyTable>(ComponentParameter.CreateParameter("Surveys", testSurvey));
+
+                var rows = surveyTableComponent.FindAll("tr.bg-white");
+                // Assert.Equal(testSurvey.Count, rows.Count); - For testing purposes it is commented out once done uncomment
+
+                foreach (var survey in testSurvey)
+                {
+                    Assert.NotNull(rows.FirstOrDefault(element => element.ToMarkup().Contains(survey.Title), null));
+                    Assert.NotNull(rows.FirstOrDefault(element => element.ToMarkup().Contains(survey.Deadline.Date.ToString("dd/MM/yyyy")), null));
+                    Assert.NotNull(rows.FirstOrDefault(element => element.ToMarkup().Contains(survey.Template.Name), null));
+                    Assert.NotNull(rows.FirstOrDefault(element => element.ToMarkup().Contains(survey.Coach.Name), null));
+                    Assert.NotNull(rows.FirstOrDefault(element => element.ToMarkup().Contains(Status.Draft.ToString()), null));
+                }
+        }
 }
