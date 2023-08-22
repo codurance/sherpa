d2076e9ef84e5725c71a8ce788fb1581201a9e14
Arturo Menendez <arturo.menendez@codurance.com>
2023-08-22 16:58:02 +0200

    feat: acceptance test to add a new team member to an existing team

--- SherpaBackEnd.Tests/Acceptance/TeamMemberAcceptanceTest.cs
+++ SherpaBackEnd.Tests/Acceptance/TeamMemberAcceptanceTest.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using SherpaBackEnd.Controllers;
@@ -7,18 +9,30 @@ using SherpaBackEnd.Services;
 
 namespace SherpaBackEnd.Tests.Acceptance;
 
-public class TeamMembersAcceptanceTest
+public class TeamMemberAcceptanceTest
 {
     [Fact]
     public async Task ShouldBeAbleToAddATeamMemberToAnExistingTeamAndGetUpdatedListOfTeamMembers()
     {
         const string teamName = "New team";
         var teamId = Guid.NewGuid();
-        var newTeam = new Team( teamId,teamName);
-        var teamsList = new List<Team>(){newTeam};
+        var newTeam = new Team(teamId, teamName);
+        var memberId = Guid.NewGuid();
+        var newTeamMember = new TeamMember(memberId, "New Member", "Developer", "JohnDoe@google.com");
+
+        var teamsList = new List<Team>() { newTeam };
         var inMemoryTeamRepository = new InMemoryTeamRepository(teamsList);
         var teamMemberService = new TeamMemberService(inMemoryTeamRepository);
         var logger = Mock.Of<ILogger<TeamController>>();
-        var teamController = new TeamController(teamService, logger);
+        var teamMemberController = new TeamMemberController(teamMemberService, logger);
+
+        await teamMemberController.AddTeamMemberToTeamAsync(teamId, newTeamMember);
+
+        var currentTeamMembers = await teamMemberController.GetAllTeamMembersAsync();
+        
+        var okObjectResult = Assert.IsType<OkObjectResult>(currentTeamMembers.Result);
+        Assert.Equal(StatusCodes.Status200OK, okObjectResult.StatusCode);
+        var teamMembersList = Assert.IsType<List<TeamMember>>(okObjectResult.Value);
+        Assert.Contains(newTeamMember, teamMembersList); 
     }
 }
