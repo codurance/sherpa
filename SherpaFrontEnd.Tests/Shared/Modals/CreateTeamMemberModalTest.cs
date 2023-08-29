76552bbc16c7c8e69c3728ea7a45c8d2279dfd63
Moises Rodriguez <moisesrj97@gmail.com>
2023-08-29 09:39:33 +0200

    feat: redirect to error page if error while adding member

--- SherpaFrontEnd.Tests/Shared/Modals/CreateTeamMemberModalTest.cs
+++ SherpaFrontEnd.Tests/Shared/Modals/CreateTeamMemberModalTest.cs
@@ -1,5 +1,6 @@
 using BlazorApp.Tests.Helpers.Interfaces;
 using Bunit;
+using Bunit.TestDoubles;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
@@ -14,12 +15,14 @@ public class CreateTeamMemberModalTest
 {
     private TestContext _ctx;
     private Mock<IGuidService> _mockGuidService;
+    private readonly FakeNavigationManager _navMan;
 
     public CreateTeamMemberModalTest()
     {
         _ctx = new TestContext();
         _mockGuidService = new Mock<IGuidService>();
         _ctx.Services.AddSingleton<IGuidService>(_mockGuidService.Object);
+        _navMan = _ctx.Services.GetService<FakeNavigationManager>();
     }
 
     [Fact]
@@ -261,5 +264,57 @@ public class CreateTeamMemberModalTest
         var emailLabel = cut.FindAll("label")
             .FirstOrDefault(element => element.InnerHtml.Contains("Email"));
         Assert.Contains("This field is mandatory", emailLabel.Parent.ToMarkup());
+    }
+    
+    [Fact]
+    public void ShouldRedirectToErrorPageIfThereIsAnErrorCreatingTheMember()
+    {
+        var teamId = Guid.NewGuid();
+        var teamMember = new TeamMember(Guid.NewGuid(), "Full name", "Some position", "demo@demo.com");
+        var mockWithCreateTeamMember = new Mock<IWithCreateTeamMember>();
+        var mockWithCloseModal = new Mock<IWithCloseModal>();
+
+        _mockGuidService.Setup(service => service.GenerateRandomGuid()).Returns(teamMember.Id);
+        mockWithCreateTeamMember.Setup(service => service.CreateTeamMember(It.IsAny<AddTeamMemberDto>()))
+            .Throws(new Exception());
+
+        var cut = _ctx.RenderComponent<CreateTeamMemberModal>(
+            ComponentParameter.CreateParameter("Show", true),
+            ComponentParameter.CreateParameter("TeamId", teamId),
+            ComponentParameter.CreateParameter("CreateTeamMember",
+                EventCallback.Factory.Create<AddTeamMemberDto>(this,
+                    async (AddTeamMemberDto a) => await mockWithCreateTeamMember.Object.CreateTeamMember(a))),
+            ComponentParameter.CreateParameter("CloseModal",
+                EventCallback.Factory.Create(this, () => mockWithCloseModal.Object.CloseModal())
+            ));
+
+        var fullNameLabel = cut.FindAll("label")
+            .FirstOrDefault(element => element.InnerHtml.Contains("Full name"));
+        var fullNameInputId = fullNameLabel.Attributes.GetNamedItem("for");
+        var teamMemberNameInput = cut.Find($"#{fullNameInputId.TextContent}");
+        Assert.NotNull(teamMemberNameInput);
+        teamMemberNameInput.Change(teamMember.FullName);
+
+        var positionLabel = cut.FindAll("label")
+            .FirstOrDefault(element => element.InnerHtml.Contains("Position"));
+        var positionInputId = positionLabel.Attributes.GetNamedItem("for");
+        var positionInput = cut.Find($"#{positionInputId.TextContent}");
+        Assert.NotNull(positionInput);
+        positionInput.Change(teamMember.Position);
+
+        var emailLabel = cut.FindAll("label")
+            .FirstOrDefault(element => element.InnerHtml.Contains("Email"));
+        var emailInputId = emailLabel.Attributes.GetNamedItem("for");
+        var emailInput = cut.Find($"#{emailInputId.TextContent}");
+        Assert.NotNull(emailInput);
+        emailInput.Change(teamMember.Email);
+
+        var addMemberButton = cut.FindAll("button")
+            .FirstOrDefault(element => element.InnerHtml.Contains("Add member"));
+        Assert.NotNull(addMemberButton);
+
+        addMemberButton.Click();
+
+        cut.WaitForAssertion(() => Assert.Equal("http://localhost/error", _navMan.Uri));
     }
 }
