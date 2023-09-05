a1ada54ddfad1847ca4a9e0e3af86dd517f05527
kleov-rf <kleonardoruizflorez@gmail.com>
2023-09-05 16:48:47 +0200

    fix: passed failing teams acceptance tests in order to use persistance layer

--- SherpaBackEnd.Tests/Acceptance/TeamsAcceptanceTest.cs
+++ SherpaBackEnd.Tests/Acceptance/TeamsAcceptanceTest.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Options;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using Moq;
+using Shared.Test.Helpers;
 using SherpaBackEnd.Controllers;
 using SherpaBackEnd.Dtos;
 using SherpaBackEnd.Model;
@@ -21,14 +22,15 @@ public class TeamsAcceptanceTest
         .WithImage("mongodb/mongodb-community-server:latest")
         .WithPortBinding(27017, true).Build();
 
-    private readonly IOptions<DatabaseSettings> _databaseSettings;
-    private readonly IMongoCollection<BsonDocument> _surveyCollection;
-    private readonly IMongoCollection<BsonDocument> _teamCollection;
-    private readonly IMongoCollection<BsonDocument> _teamMemberCollection;
-    private readonly IMongoCollection<BsonDocument> _templateCollection;
+    private IOptions<DatabaseSettings> _databaseSettings;
+    private IMongoCollection<BsonDocument> _surveyCollection;
+    private IMongoCollection<BsonDocument> _teamCollection;
+    private IMongoCollection<BsonDocument> _teamMemberCollection;
+    private IMongoCollection<BsonDocument> _templateCollection;
     
-    public TeamsAcceptanceTest()
+    private async Task InitializeDbClientAndCollections()
     {
+        await _mongoDbContainer.StartAsync();
         _databaseSettings = Options.Create(new DatabaseSettings
         {
             DatabaseName = "Sherpa",
@@ -38,14 +40,11 @@ public class TeamsAcceptanceTest
             TemplateCollectionName = "Templates",
             ConnectionString = $"mongodb://localhost:{_mongoDbContainer.GetMappedPublicPort(27017)}"
         });
-        
+
         var mongoClient = new MongoClient(_databaseSettings.Value.ConnectionString);
 
         var mongoDatabase = mongoClient.GetDatabase(
             _databaseSettings.Value.DatabaseName);
-
-        _surveyCollection = mongoDatabase.GetCollection<BsonDocument>(
-            _databaseSettings.Value.SurveyCollectionName);
 
         _teamCollection = mongoDatabase.GetCollection<BsonDocument>(
             _databaseSettings.Value.TeamsCollectionName);
@@ -55,12 +54,16 @@ public class TeamsAcceptanceTest
 
         _templateCollection = mongoDatabase.GetCollection<BsonDocument>(
             _databaseSettings.Value.TemplateCollectionName);
+        
+        _surveyCollection = mongoDatabase.GetCollection<BsonDocument>(
+            _databaseSettings.Value.SurveyCollectionName);
     }
-    
+
     [Fact]
     public async Task ShouldBeAbleToCreateTeamAndGetUpdatedListOfTeamsWithNewOne()
     {
-        var emptyTeamsList = new List<Team>();
+        await InitializeDbClientAndCollections();
+        
         var inMemoryTeamRepository = new MongoTeamRepository(_databaseSettings);
         var teamService = new TeamService(inMemoryTeamRepository);
         var logger = Mock.Of<ILogger<TeamController>>();
@@ -73,16 +76,42 @@ public class TeamsAcceptanceTest
 
         var actualTeams = await teamController.GetAllTeamsAsync();
         var okObjectResult = Assert.IsType<OkObjectResult>(actualTeams.Result);
-        Assert.Equal(emptyTeamsList, okObjectResult.Value);
+        Assert.Equal(StatusCodes.Status200OK, okObjectResult.StatusCode);
+        var teams = Assert.IsAssignableFrom<IEnumerable<Team>>(okObjectResult.Value);
+        Assert.Contains(teams, team => team.Id == newTeam.Id);
     }
     
     [Fact]
     public async Task ShouldBeAbleToRetrieveASingleTeamById()
     {
+        await InitializeDbClientAndCollections();
+        
         const string teamName = "Demo team";
         var teamId = Guid.NewGuid();
-        var expectedTeam = new Team(teamId, teamName );
-        var teamsList = new List<Team>() { expectedTeam };
+        
+        
+        var memberId = Guid.NewGuid();
+        var newTeamMember = new TeamMember(memberId, "New Member", "Developer", "JohnDoe@google.com");
+        
+        await _teamMemberCollection.InsertOneAsync(
+            new BsonDocument() {
+                { "_id", memberId.ToString() },
+                { "FullName", newTeamMember.FullName },
+                { "Position", newTeamMember.Position },
+                { "Email", newTeamMember.Email },
+            });
+        
+        var expectedTeam = new Team(teamId, teamName, new List<TeamMember>(){newTeamMember});
+        
+        await _teamCollection.InsertOneAsync(
+            new BsonDocument
+            {
+                { "_id", teamId.ToString() },
+                { "Name", teamName },
+                { "Members", new BsonArray() {memberId.ToString()} },
+                { "IsDeleted", false }
+            });
+
         var inMemoryTeamRepository = new MongoTeamRepository(_databaseSettings);
         var teamService = new TeamService(inMemoryTeamRepository);
         var logger = Mock.Of<ILogger<TeamController>>();
@@ -92,6 +121,6 @@ public class TeamsAcceptanceTest
         
         var resultObject = Assert.IsType<OkObjectResult>(teamByIdAsync.Result);
         Assert.Equal(StatusCodes.Status200OK, resultObject.StatusCode);
-        Assert.Equal(expectedTeam, resultObject.Value);
+        CustomAssertions.StringifyEquals(expectedTeam, resultObject.Value);
     }
 }
