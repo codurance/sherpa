03760952b4f11d699b4a4b184e06f0ac57732693
Moises Rodriguez <moisesrj97@gmail.com>
2023-09-08 10:26:04 +0200

    feat: auth working

--- SherpaFrontEnd/Program.cs
+++ SherpaFrontEnd/Program.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using Blazored.Modal;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SherpaFrontEnd;
 using SherpaFrontEnd.Serializers;
@@ -12,6 +13,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("SherpaBackEnd", client =>
     client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.Configure<JsonSerializerOptions>(options =>
 {
@@ -23,7 +25,14 @@ builder.Services.AddScoped<ITeamDataService, TeamServiceHttpClient>();
 builder.Services.AddScoped<ITemplateService, TemplateService>();
 builder.Services.AddScoped<IGuidService, GuidService>();
 builder.Services.AddScoped<ISurveyService, SurveyService>();
-
 builder.Services.AddBlazoredModal();
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    // Configure your authentication provider options here.
+    // For more information, see https://aka.ms/blazor-standalone-auth
+    builder.Configuration.Bind("Cognito", options.ProviderOptions);
+});
+
 
 await builder.Build().RunAsync();
