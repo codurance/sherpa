b73900a0f89f7047fe09cafe6a3120f7ed416329
Benjamin Rae <benjaminrae93@gmail.com>
2024-06-03 12:50:36 +0200

    fix: line endings in SurveyCopy

--- SherpaFrontEnd/Pages/DeliverySettings/SurveyCopy.cs
+++ SherpaFrontEnd/Pages/DeliverySettings/SurveyCopy.cs
@@ -6,17 +6,7 @@ public class SurveyCopy
 
     public static string DefaultDescription()
     {
-        return
-            @"Hi team! This questionnaire is part of an overall team assessment. It focuses on describing how we are currently working as a team. The important part about this survey is not the answers themselves, but the conversations that we can have around based on the results obtained. To consider:
-
-- It takes at least 30 min to complete
-- It contains around 100 questions, most of them on a scale of 1 to 5 that represents how much you disagree (""strongly disagree"") or agree (""strongly agree"") with what was stated.
-- Saving questions halfway is not allowed
-
-Please try to be as honest as possible. Your answers are very important and will help the team.
-
-Thank you!
-
-- Codurance Team";
+        return "Hi team! This questionnaire is part of an overall team assessment. It focuses on describing how we are currently working as a team. The important part about this survey is not the answers themselves, but the conversations that we can have around based on the results obtained. To consider:\n\n- It takes at least 30 min to complete\n- It contains around 100 questions, most of them on a scale of 1 to 5 that represents how much you disagree (\"\"strongly disagree\"\") or agree (\"\"strongly agree\"\") with what was stated.\n- Saving questions halfway is not allowed\n\nPlease try to be as honest as possible. Your answers are very important and will help the team.\n\nThank you!\n\n- Codurance Team";
+                
     }
 }
