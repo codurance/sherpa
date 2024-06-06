e7bc2f5e14f8b2f60a9f12eac19706d5bc8d7b1a
Paula Masutier <paulamasutier@gmail.com>
2024-06-06 13:13:11 +0200

    feat: create email templates from survey notifications

--- SherpaBackEnd/Email/Application/EmailTemplateFactory.cs
+++ SherpaBackEnd/Email/Application/EmailTemplateFactory.cs
@@ -2,8 +2,11 @@ namespace SherpaBackEnd.Email.Application;
 
 public class EmailTemplateFactory : IEmailTemplateFactory
 {
+    private string _baseAnswerSurveyUrl = "sherpa.com/answer-survey/";
+
     public List<EmailTemplate> CreateEmailTemplate(List<SurveyNotification.Domain.SurveyNotification> surveyNotifications)
     {
-        throw new NotImplementedException();
+        return surveyNotifications.Select(notification =>
+            new EmailTemplate(notification.TeamMember.Email, _baseAnswerSurveyUrl + notification.Id)).ToList();
     }
 }
--- SherpaBackEnd/Email/EmailTemplate.cs
+++ SherpaBackEnd/Email/EmailTemplate.cs
@@ -10,4 +10,22 @@ public class EmailTemplate
         Recipient = recipient;
         Url = url;
     }
+
+    protected bool Equals(EmailTemplate other)
+    {
+        return Recipient == other.Recipient && Url == other.Url;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((EmailTemplate)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Recipient, Url);
+    }
 }
