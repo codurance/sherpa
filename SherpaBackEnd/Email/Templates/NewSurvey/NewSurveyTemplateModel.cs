507d36253d2973bef8c6b34058a0b5d7274e8224
Paula Masutier <paulamasutier@gmail.com>
2024-06-19 19:23:51 +0200

    feat: model for NewSurveyTemplateModel

--- SherpaBackEnd/Email/Templates/NewSurvey/NewSurveyTemplateModel.cs
+++ SherpaBackEnd/Email/Templates/NewSurvey/NewSurveyTemplateModel.cs
@@ -4,6 +4,6 @@ public class NewSurveyTemplateModel
 {
     public string Name { get; set; }
     public string SurveyName { get; set; }
-    public DateTime Deadline { get; set; }
+    public DateTime? Deadline { get; set; }
     public string Url { get; set; }
 }
