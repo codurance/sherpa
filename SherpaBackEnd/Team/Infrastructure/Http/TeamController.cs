2d3c10d7a46fb0af6a2ed3af7964ba33cbea155e
Noel Lopez <nlopez.publi@gmail.com>
2024-06-26 13:39:39 +0200

    feat: Add authorization PoC in backend teams endpoint

--- SherpaBackEnd/Team/Infrastructure/Http/TeamController.cs
+++ SherpaBackEnd/Team/Infrastructure/Http/TeamController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SherpaBackEnd.Team.Application;
 
 namespace SherpaBackEnd.Team.Infrastructure.Http;
 
+[Authorize]
 [ApiController]
 [Route("[controller]")]
 public class TeamController
