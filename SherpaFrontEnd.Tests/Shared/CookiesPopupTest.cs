29b4862a783da4b952c94620d73bdcf04724cb31
Noel Lopez <nlopez.publi@gmail.com>
2024-07-01 13:01:03 +0200

    test: Cookies popup should not show if cookies are already accepted

--- SherpaFrontEnd.Tests/Shared/CookiesPopupTest.cs
+++ SherpaFrontEnd.Tests/Shared/CookiesPopupTest.cs
@@ -11,6 +11,25 @@ public class CookiesPopupTest
 {
 
     [Fact]
+    public void ShouldNotShowIfCookiesAreAlreadyAccepted()
+    {
+        var testContext = new TestContext();
+        var mockCookiesService = new Mock<ICookiesService>();
+
+        mockCookiesService.Setup(mock => mock.AreCookiesAccepted()).ReturnsAsync(true);
+        
+        testContext.Services.AddScoped<ICookiesService>(sp => mockCookiesService.Object);
+        
+        var cut = testContext.RenderComponent<CookiesPopup>();
+        
+        var cookiesPopup = cut.FindElementByCssSelectorAndTextContent("div[role=\"dialog\"]", "To find out more about our Cookies policy here. Once you are done, please, come back and accept them.");
+        
+        mockCookiesService.Verify(mock => mock.AreCookiesAccepted(), Times.Once);
+        
+        AssertPopupIsNotShown(cookiesPopup);
+    }
+
+    [Fact]
     public void ShouldShowIfCookiesAreNotAccepted()
     {
         var testContext = new TestContext();
