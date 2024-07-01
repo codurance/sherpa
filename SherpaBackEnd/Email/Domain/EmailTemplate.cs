aeba97f6cc77ff2e78e328a06f67003e14589477
Paula Masutier <paulamasutier@gmail.com>
2024-07-01 15:30:42 +0200

    refactor: add Subject property to EmailTemplate and change Subject and TemplateName on SesEmailService

--- SherpaBackEnd/Email/Domain/EmailTemplate.cs
+++ SherpaBackEnd/Email/Domain/EmailTemplate.cs
@@ -5,12 +5,14 @@ namespace SherpaBackEnd.Email;
 public class EmailTemplate
 {
     public string TemplateName { get; }
+    public string Subject { get; }
 
     public List<Recipient> Recipients { get; }
 
-    public EmailTemplate(string templateName, List<Recipient> recipients)
+    public EmailTemplate(string templateName, string subject, List<Recipient> recipients)
     {
         TemplateName = templateName;
+        Subject = subject;
         Recipients = recipients;
     }
 }
