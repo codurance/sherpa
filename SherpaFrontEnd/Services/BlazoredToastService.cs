976f263a730abe3d1c2f072bf1a787e66840715f
Noel Lopez <nlopez.publi@gmail.com>
2024-07-03 16:24:18 +0200

    feat: BlazoredToastService should show a success toast notification

---
+++ SherpaFrontEnd/Services/BlazoredToastService.cs
@@ -0,0 +1,18 @@
+﻿using Blazored.Toast.Services;
+
+namespace SherpaFrontEnd.Services;
+
+public class BlazoredToastService : IToastNotificationService
+{
+    private readonly IToastService _toastService;
+
+    public BlazoredToastService(IToastService toastService)
+    {
+        _toastService = toastService;
+    }
+
+    public void ShowSuccess(string message)
+    {
+        _toastService.ShowSuccess(message);
+    }
+}
