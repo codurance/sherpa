e84ce5303401f59478d489ea90ca01eb68b304c8
Noel Lopez <82571330+noel-lopez@users.noreply.github.com>
2024-07-04 11:42:52 +0200

    feat(SSB-89): success toast notifications (#54)
    * test: start acceptance test for toast on add team member
    
    * test: acceptance test for toast on add team member
    
    * test: fix previous acceptance tests adding toasts service
    
    * feat: BlazoredToastService should show a success toast notification
    
    * feat: show a success toast when team member added to team
    
    * refactor: initialize BlazoredToastService in acceptance test
    
    * test: fix previous acceptance tests adding BlazoredToastService
    
    * feat: show success toast notification when launching survey
    
    * feat: show success toast notification when team is created
    
    ---------
    
    Co-authored-by: Benjamin Rae <benjaminrae93@gmail.com>

---
+++ SherpaFrontEnd.Tests/Services/BlazoredToastServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Blazored.Toast.Configuration;
+using Blazored.Toast.Services;
+using Moq;
+using SherpaFrontEnd.Services;
+
+namespace BlazorApp.Tests.Services;
+
+public class BlazoredToastServiceTest
+{
+
+    [Fact]
+    public void ShouldShowASuccessToastNotification()
+    {
+        var toastMock = new Mock<IToastService>();
+        
+        var blazoredToastService = new BlazoredToastService(toastMock.Object);
+
+        blazoredToastService.ShowSuccess("Test 1");
+        
+        toastMock.Verify(service => service.ShowSuccess("Test 1", It.IsAny<Action<ToastSettings>>()));
+    }
+}
