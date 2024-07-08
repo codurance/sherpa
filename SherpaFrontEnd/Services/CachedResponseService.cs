8069f8d064b2cec7f466ed435471c8b8376448f7
Noel Lopez <nlopez.publi@gmail.com>
2024-07-08 13:22:42 +0200

    refactor: use null coalescing operator

--- SherpaFrontEnd/Services/CachedResponseService.cs
+++ SherpaFrontEnd/Services/CachedResponseService.cs
@@ -14,11 +14,6 @@ public class CachedResponseService : ICachedResponseService
     public async Task<Dictionary<int, string>> GetBy(Guid surveyNotificationId)
     {
         var cachedResponses = await _localStorageService.GetItemAsync<Dictionary<int, string>>($"response-{surveyNotificationId}");
-        if (cachedResponses == null)
-        {
-            return new Dictionary<int, string>();
-        }
-
-        return cachedResponses;
+        return cachedResponses ?? new Dictionary<int, string>();
     }
 }
