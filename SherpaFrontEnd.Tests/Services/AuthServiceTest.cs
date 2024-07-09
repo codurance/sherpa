9b63a36d3f1014cc8d75f2c6e954b7b723d62d7a
fahadCodurance <fahad.badar@codurance.com>
2024-07-09 12:33:00 +0100

    test: redirect to log in page when token is expired

--- SherpaFrontEnd.Tests/Services/AuthServiceTest.cs
+++ SherpaFrontEnd.Tests/Services/AuthServiceTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+﻿using Bunit.TestDoubles;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Moq;
 using SherpaFrontEnd.Services;
 
@@ -11,6 +13,7 @@ public class AuthServiceTest
     public async Task ShouldAddTokenToRequest()
     {
         var tokenProviderMock = new Mock<IAccessTokenProvider>();
+        var navigationManagerMock = new Mock<FakeNavigationManager>();
         var expectedToken = new AccessToken()
         {
             Value = "test"
@@ -18,11 +21,26 @@ public class AuthServiceTest
         var accessTokenMock = new AccessTokenResult(AccessTokenResultStatus.Success, expectedToken, "");
         tokenProviderMock.Setup(tokenProvider => tokenProvider.RequestAccessToken()).ReturnsAsync(accessTokenMock);
         
-        var authService = new AuthService(tokenProviderMock.Object);
+        var authService = new AuthService(tokenProviderMock.Object, navigationManagerMock.Object);
 
         var request = new HttpRequestMessage();
         request = await authService.DecorateWithToken(request);
         
         Assert.Equal(expectedToken.Value, request.Headers.Authorization?.Parameter);
+    }
+
+
+    [Fact(Skip = "")]
+
+    public async Task ShouldRedirectToLoginPageWhenTokenIsExpired()
+    {
+        var tokenProviderMock = new Mock<IAccessTokenProvider>();
+        var navManagerMock = new Mock<NavigationManager>();
+        var accessTokenMock = new AccessTokenResult(AccessTokenResultStatus.RequiresRedirect, null, "");
+        var authService = new AuthService(tokenProviderMock.Object, navManagerMock.Object);
+        tokenProviderMock.Setup(tokenProvider => tokenProvider.RequestAccessToken()).ReturnsAsync(accessTokenMock);
+
+        await authService.DecorateWithToken(new HttpRequestMessage());
+        navManagerMock.Verify(navManger => navManger.NavigateTo("/authentication/logged-out", It.IsAny<bool>()));
     }
 }
