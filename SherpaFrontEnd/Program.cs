a2c1261f277a1f0fa3e774c8dd39accc22ba7361
Noel Lopez <nlopez.publi@gmail.com>
2024-07-10 17:30:03 +0200

    chore: use environment variables in frontend Program.cs

--- SherpaFrontEnd/Program.cs
+++ SherpaFrontEnd/Program.cs
@@ -5,7 +5,6 @@ using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SherpaFrontEnd;
-using SherpaFrontEnd.Dtos;
 using SherpaFrontEnd.Serializers;
 using SherpaFrontEnd.Services;
 
@@ -17,10 +16,7 @@ builder.Services.AddHttpClient("SherpaBackEnd", client =>
     client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.Configure<JsonSerializerOptions>(options =>
-{
-    options.Converters.Add(new DateOnlyJsonConverter());
-});
+builder.Services.Configure<JsonSerializerOptions>(options => { options.Converters.Add(new DateOnlyJsonConverter()); });
 
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<INavigationService, NavigationService>();
@@ -35,23 +31,18 @@ builder.Services.AddBlazoredModal();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazoredToast();
 
-// POC
-var client = new HttpClient(){BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)};
-var httpRequestMessage =
-    new HttpRequestMessage(HttpMethod.Get, $"/survey/44177fd8-685f-4a13-9c88-e2a75f84eaec");
+var client = new HttpClient() { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+var environmentVariablesService = new EnvironmentVariablesService(client);
+var environmentVariables = await environmentVariablesService.GetVariables();
 
-var httpResponseMessage = await client.SendAsync(httpRequestMessage);
-var responseString = await httpResponseMessage.Content.ReadAsStringAsync();
-
-var result = JsonSerializer.Deserialize<SurveyWithoutQuestions>(responseString,
-    new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
-Console.WriteLine("ClientID: " + builder.Configuration["Cognito:ClientId"]);
 builder.Services.AddOidcAuthentication(options =>
 {
-    options.ProviderOptions.Authority = builder.Configuration["Cognito:Authority"];
-    options.ProviderOptions.ClientId = builder.Configuration["Cognito:ClientId"];
-    options.ProviderOptions.RedirectUri = builder.HostEnvironment.BaseAddress + builder.Configuration["Cognito:RedirectUri"];
-    options.ProviderOptions.PostLogoutRedirectUri = builder.HostEnvironment.BaseAddress + builder.Configuration["Cognito:PostLogoutRedirectUri"];
+    options.ProviderOptions.Authority = environmentVariables?.CognitoAuthority;
+    options.ProviderOptions.ClientId = environmentVariables?.CognitoClientId;
+    options.ProviderOptions.RedirectUri =
+        builder.HostEnvironment.BaseAddress + builder.Configuration["Cognito:RedirectUri"];
+    options.ProviderOptions.PostLogoutRedirectUri = builder.HostEnvironment.BaseAddress +
+                                                    builder.Configuration["Cognito:PostLogoutRedirectUri"];
     options.ProviderOptions.ResponseType = "code";
 });
 await builder.Build().RunAsync();
