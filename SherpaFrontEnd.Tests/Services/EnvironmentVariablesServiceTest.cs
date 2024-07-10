501e04f1ca14cd2f04fd62f881af2e755776fe3e
Noel Lopez <nlopez.publi@gmail.com>
2024-07-10 17:29:12 +0200

    feat: frontend get environment variables from backend

---
+++ SherpaFrontEnd.Tests/Services/EnvironmentVariablesServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Moq;
+using Shared.Test.Helpers;
+using SherpaFrontEnd.Dtos;
+using SherpaFrontEnd.Services;
+
+namespace BlazorApp.Tests.Services;
+
+public class EnvironmentVariablesServiceTest
+{
+    private readonly Mock<HttpMessageHandler> _handlerMock;
+    private readonly HttpClient _httpClient;
+
+    public EnvironmentVariablesServiceTest()
+    {
+        _handlerMock = new Mock<HttpMessageHandler>();
+        _httpClient = new HttpClient(_handlerMock.Object, false) { BaseAddress = new Uri("http://host") };
+    }
+
+    [Fact]
+    public async Task ShouldGetEnvironmentVariables()
+    {
+        var environmentVariables = new EnvironmentVariables("clientId", "authority123");
+        var environmentVariablesJson = await JsonContent.Create(environmentVariables).ReadAsStringAsync();
+
+        var environmentVariablesResponse = new HttpResponseMessage
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = new StringContent(environmentVariablesJson)
+        };
+        _handlerMock.SetupRequest(HttpMethod.Get, "/configuration", environmentVariablesResponse);
+
+        var environmentVariablesService = new EnvironmentVariablesService(_httpClient);
+        var actualEnvironmentVariables = await environmentVariablesService.GetVariables();
+        CustomAssertions.StringifyEquals(environmentVariables, actualEnvironmentVariables);
+    }
+}
---
+++ SherpaFrontEnd/Services/EnvironmentVariablesService.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using SherpaFrontEnd.Dtos;
+
+namespace SherpaFrontEnd.Services;
+
+public class EnvironmentVariablesService
+{
+    private readonly HttpClient _httpClient;
+
+    public EnvironmentVariablesService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<EnvironmentVariables?> GetVariables()
+    {
+        var httpRequestMessage =
+            new HttpRequestMessage(HttpMethod.Get, "/configuration");
+
+        var httpResponseMessage = await _httpClient.SendAsync(httpRequestMessage);
+        var responseString = await httpResponseMessage.Content.ReadAsStringAsync();
+
+        return JsonSerializer.Deserialize<EnvironmentVariables>(responseString,
+            new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+    }
+}
