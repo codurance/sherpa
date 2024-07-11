38e0c799dfcad2bcc90190f870dd7ae65b66af32
fahadCodurance <91265612+fahadCodurance@users.noreply.github.com>
2024-07-11 11:10:20 +0100

    feat(SSB-122): redirect to login page when token is expired (#58)
    * test: redirect to log in page when token is expired
    
    * feat: redirect to login page when token is expired
    
    * feat: navigation service
    
    * test: verifies getting current uri
    
    * test: verify navigate to full url
    
    ---------
    
    Co-authored-by: Paula Masutier <paulamasutier@gmail.com>
    Co-authored-by: Noel Lopez <nlopez.publi@gmail.com>

--- SherpaFrontEnd.Tests/Services/AuthServiceTest.cs
+++ SherpaFrontEnd.Tests/Services/AuthServiceTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+﻿using Bunit.TestDoubles;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Moq;
 using SherpaFrontEnd.Services;
 
@@ -18,11 +20,29 @@ public class AuthServiceTest
         var accessTokenMock = new AccessTokenResult(AccessTokenResultStatus.Success, expectedToken, "");
         tokenProviderMock.Setup(tokenProvider => tokenProvider.RequestAccessToken()).ReturnsAsync(accessTokenMock);
         
-        var authService = new AuthService(tokenProviderMock.Object);
+        var authService = new AuthService(tokenProviderMock.Object, null);
 
         var request = new HttpRequestMessage();
         request = await authService.DecorateWithToken(request);
         
         Assert.Equal(expectedToken.Value, request.Headers.Authorization?.Parameter);
+    }
+    
+    [Fact]
+    public async Task ShouldRedirectToLoginPageWhenTokenIsExpired1()
+    {
+        var tokenProviderMock = new Mock<IAccessTokenProvider>();
+        var navigationService = new Mock<INavigationService>();
+
+        var accessTokenMock = new AccessTokenResult(AccessTokenResultStatus.RequiresRedirect, null, "");
+        tokenProviderMock.Setup(tp => tp.RequestAccessToken()).ReturnsAsync(accessTokenMock);
+        navigationService.SetupGet(ns => ns.CurrentUri).Returns("http://localhost/teams-list");
+
+        var authService = new AuthService(tokenProviderMock.Object, navigationService.Object);
+
+        await authService.DecorateWithToken(new HttpRequestMessage());
+
+        var expectedLoginUrl = "authentication/login?returnUrl=http%3A%2F%2Flocalhost%2Fteams-list";
+        navigationService.Verify(service => service.NavigateTo(expectedLoginUrl), Times.Once);
     }
 }
--- SherpaFrontEnd/Services/AuthService.cs
+++ SherpaFrontEnd/Services/AuthService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 namespace SherpaFrontEnd.Services;
@@ -6,16 +7,23 @@ namespace SherpaFrontEnd.Services;
 public class AuthService : IAuthService
 {
     private readonly IAccessTokenProvider _tokenProvider;
+    private INavigationService  _navigationService;
 
-    public AuthService(IAccessTokenProvider tokenProvider)
+    public AuthService(IAccessTokenProvider tokenProvider, INavigationService navigationService)
     {
         _tokenProvider = tokenProvider;
+        _navigationService = navigationService;
     }
 
     public async Task<HttpRequestMessage> DecorateWithToken(HttpRequestMessage request)
     {
         var requestAccessToken = await _tokenProvider.RequestAccessToken();
         requestAccessToken.TryGetToken(out var token);
+        if (token == null)
+        {
+            _navigationService.NavigateTo($"authentication/login?returnUrl={Uri.EscapeDataString(_navigationService.CurrentUri)}");
+            return request;
+        }
 
         request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token.Value);
 
