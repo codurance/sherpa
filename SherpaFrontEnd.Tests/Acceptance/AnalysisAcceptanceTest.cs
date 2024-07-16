0b38b76f6b25b383a1ab5970e8ff26b0d907910d
Noel Lopez <nlopez.publi@gmail.com>
2024-07-16 11:57:41 +0200

    test: fix acceptance test taking into account JSInterop limitations

--- SherpaFrontEnd.Tests/Acceptance/AnalysisAcceptanceTest.cs
+++ SherpaFrontEnd.Tests/Acceptance/AnalysisAcceptanceTest.cs
@@ -9,6 +9,7 @@ using Bunit;
 using Bunit.TestDoubles;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using Shared.Test.Helpers;
 using SherpaFrontEnd;
 using SherpaFrontEnd.Dtos.Analysis;
 using SherpaFrontEnd.Dtos.Survey;
@@ -22,7 +23,7 @@ public class AnalysisAcceptanceTest
     private TestContext _testCtx;
     private FakeNavigationManager _navMan;
     private Mock<HttpMessageHandler> _httpHandlerMock;
-    private Mock<IHttpClientFactory>  _factoryHttpClient;
+    private Mock<IHttpClientFactory> _factoryHttpClient;
     private Mock<IAuthService> _authService;
 
     public AnalysisAcceptanceTest()
@@ -49,9 +50,9 @@ public class AnalysisAcceptanceTest
         _testCtx.Services.AddSingleton<IHttpClientFactory>(_factoryHttpClient.Object);
         var auth = _testCtx.AddTestAuthorization();
         auth.SetAuthorized("Demo user");
-        auth.SetClaims(new []{new Claim("username", "Demo user")});
+        auth.SetClaims(new[] { new Claim("username", "Demo user") });
         _testCtx.JSInterop.SetupVoid("generateColumnsChart", _ => true);
-        _navMan =  _testCtx.Services.GetRequiredService<FakeNavigationManager>();
+        _navMan = _testCtx.Services.GetRequiredService<FakeNavigationManager>();
     }
 
     [Fact]
@@ -60,29 +61,29 @@ public class AnalysisAcceptanceTest
         const string newTeamName = "Team with surveys";
         var newTeamId = Guid.NewGuid();
         var newTeam = new Team(newTeamId, newTeamName);
-        
-        
+
+
         var singleTeamJson = await JsonContent.Create(newTeam).ReadAsStringAsync();
         var singleTeamResponse = new HttpResponseMessage()
         {
             StatusCode = HttpStatusCode.OK,
             Content = new StringContent(singleTeamJson)
         };
-        
+
         _httpHandlerMock.SetupRequest(HttpMethod.Get, $"/team/{newTeamId.ToString()}", singleTeamResponse);
 
         var userOne = new User(Guid.NewGuid(), "user");
         var survey = new Survey(Guid.NewGuid(), userOne, Status.InProgress, new DateTime(), "title", "description",
-            new []{new Response()}, newTeam, new Template("template"));
-        var surveyList = new List<Survey>(){ survey };
+            new[] { new Response() }, newTeam, new Template("template"));
+        var surveyList = new List<Survey>() { survey };
         var surveyListJson = await JsonContent.Create(surveyList).ReadAsStringAsync();
         var surveyListResponse = new HttpResponseMessage()
         {
             StatusCode = HttpStatusCode.OK,
             Content = new StringContent(surveyListJson)
         };
-        
-        _httpHandlerMock.SetupRequest(HttpMethod.Get, $"/team/{newTeam.Id.ToString()}/surveys",surveyListResponse );
+
+        _httpHandlerMock.SetupRequest(HttpMethod.Get, $"/team/{newTeam.Id.ToString()}/surveys", surveyListResponse);
 
         var generalResults = SetupGeneralResultsDto();
 
@@ -92,22 +93,22 @@ public class AnalysisAcceptanceTest
             StatusCode = HttpStatusCode.OK,
             Content = new StringContent(generalResultsJson)
         };
-        
-        _httpHandlerMock.SetupRequest(HttpMethod.Get, $"team/{newTeam.Id}/analysis/general-results", generalResultsResponse);
-        
+
+        _httpHandlerMock.SetupRequest(HttpMethod.Get, $"team/{newTeam.Id}/analysis/general-results",
+            generalResultsResponse);
+
         var appComponent = _testCtx.RenderComponent<App>();
-        
+
         _navMan.NavigateTo($"team-content/{newTeam.Id}");
-        
+
         appComponent.WaitForAssertion(() =>
         {
             Assert.Equal($"http://localhost/team-content/{newTeam.Id}", _navMan.Uri);
         });
-        var jsRuntimeInvocation = _testCtx.JSInterop.Invocations.ToList().Find(invocation => invocation.Identifier.Equals("generateColumnsChart"));
+        var jsRuntimeInvocation = _testCtx.JSInterop.Invocations.ToList()
+            .Find(invocation => invocation.Identifier.Equals("generateColumnsChart"));
         Assert.NotNull(jsRuntimeInvocation.Identifier);
-        Assert.Contains(generalResults.ColumnChart, jsRuntimeInvocation.Arguments);
-        var firstCategory = appComponent.FindElementByCssSelectorAndTextContent("text",generalResults.ColumnChart.Categories[0]);
-        Assert.NotNull(firstCategory);
+        CustomAssertions.StringifyEquals(generalResults.ColumnChart, jsRuntimeInvocation.Arguments[1]);
     }
 
     private GeneralResultsDto SetupGeneralResultsDto()
@@ -121,20 +122,26 @@ public class AnalysisAcceptanceTest
             "Supportive org coaching"
         };
         var maxValue = 1.0;
-        
-        var firstSurvey = new ColumnSeries<double>("Survey 1", new List<double>(){ 0.5,
+
+        var firstSurvey = new ColumnSeries<double>("Survey 1", new List<double>()
+        {
             0.5,
             0.5,
             0.5,
-            0.5});
-        var secondSurvey = new ColumnSeries<double>("Survey 2", new List<double>(){ 0.5,
+            0.5,
+            0.5
+        });
+        var secondSurvey = new ColumnSeries<double>("Survey 2", new List<double>()
+        {
             0.5,
             0.5,
             0.5,
-            0.5});
-        var series = new List<ColumnSeries<double>>(){firstSurvey, secondSurvey};
+            0.5,
+            0.5
+        });
+        var series = new List<ColumnSeries<double>>() { firstSurvey, secondSurvey };
         var columnChart = new ColumnChart<double>(categories, series, maxValue);
-        
+
         var generalResults = new GeneralResultsDto(columnChart);
         return generalResults;
     }
