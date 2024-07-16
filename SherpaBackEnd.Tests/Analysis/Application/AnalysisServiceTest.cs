c21eb3c20a0fac73143c3c1ba2735875a6660132
Miguel Fernández Fuentes <miguel.fernandez@codurance.com>
2024-07-16 15:47:45 +0200

    fix: compare with stringify so comparing objects work

--- SherpaBackEnd.Tests/Analysis/Application/AnalysisServiceTest.cs
+++ SherpaBackEnd.Tests/Analysis/Application/AnalysisServiceTest.cs
@@ -1,4 +1,5 @@
 using Moq;
+using Shared.Test.Helpers;
 using SherpaBackEnd.Analysis.Application;
 using SherpaBackEnd.Analysis.Domain;
 using SherpaBackEnd.Analysis.Domain.Persistence;
@@ -16,9 +17,12 @@ public class AnalysisServiceTest
         var categories = new[]
             { "Real Team", "Compelling Direction", "Expert Coaching", "Enable Structure", "Supportive Coaching" };
         var survey1 = new ColumnSeries<double>("Survey 1", new List<double>() { 0.5, 0.5, 0.2, 0.1, 0.8 });
-        var survey2 = new ColumnSeries<double>("Survey 2", new List<double>() { 0.5, 0.5, 0.2, 0.1, 0.8 });
+        var survey2 = new ColumnSeries<double>("Survey 2", new List<double>() { 0.5, 0.6, 0.2, 0.4, 0.7 });
+        var survey3 = new ColumnSeries<double>("Survey 3", new List<double>() { 0.6, 0.6, 0.3, 0.5, 0.6 });
+        var survey4 = new ColumnSeries<double>("Survey 4", new List<double>() { 0.6, 0.6, 0.5, 0.5, 0.8 });
+        var survey5 = new ColumnSeries<double>("Survey 5", new List<double>() { 0.8, 0.7, 0.5, 0.5, 0.9 });
 
-        var series = new List<ColumnSeries<double>>() { survey1, survey2 };
+        var series = new List<ColumnSeries<double>>() { survey1, survey2, survey3, survey4, survey5 };
         var columnChart = new ColumnChart<double>(categories, series, new ColumnChartConfig<double>(1,0.25,2));
         var metrics = new Metrics(0.9,0.75);
         var expected = new GeneralResultsDto(columnChart, metrics);
@@ -45,7 +49,7 @@ public class AnalysisServiceTest
         var actual = await analysisService.GetGeneralResults(teamId);
 
         // Then
-        Assert.Equal(expected, actual);
+        CustomAssertions.StringifyEquals(expected, actual);
 
         // Surveys
         
